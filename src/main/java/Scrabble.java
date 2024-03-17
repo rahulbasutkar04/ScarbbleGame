59fa7d9451829c4496b7ec74cd056369c304adbd
rahul <115400916+rahulbasutkar04@users.noreply.github.com>
2024-03-17 10:48:04 +0530

    Added perfrom test case with method

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,2 +1,25 @@
 public class Scrabble {
+    public int perform(String input) {
+
+
+
+        String[] letters = {"EAIONRTLSU","DG","BCMP","FHVWY","K","JX","QZ"};
+        int[] points = {1, 2, 3, 4, 5, 8, 10};
+
+        int sum = 0;
+
+        for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            for(int j = 0; j < letters.length; j++) {
+                if(letters[j].contains(String.valueOf(c))) {
+                    sum += points[j];
+                    break;
+                }
+            }
+        }
+
+
+
+        return  sum;
+    }
 }
--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -1,14 +1,21 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ScrabbleTest {
     @Test
-    void shouldAbleTOCreateInstanceOfScrabbleClass(){
-        Scrabble scrabble=new Scrabble();
+    void shouldAbleTOCreateInstanceOfScrabbleClass() {
+        Scrabble scrabble = new Scrabble();
 
         assertTrue(scrabble instanceof Scrabble);
     }
 
+    @Test
+    void shouldAbleTOStartTeGame() {
+        Scrabble scrabble = new Scrabble();
+        int actual = scrabble.perform("RAHUL");
+        assertEquals(8, actual);
+    }
 
 }
